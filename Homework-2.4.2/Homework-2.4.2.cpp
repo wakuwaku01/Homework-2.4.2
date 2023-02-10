0042f1d9f203d926777be3a8c1ee6355a48bc8c7
wakuwaku01 <wakuwaku.post.mail@gmail.com>
2023-02-10 21:12:48 +0300

    change string array to object array

--- Homework-2.4.2/Homework-2.4.2.cpp
+++ Homework-2.4.2/Homework-2.4.2.cpp
@@ -4,13 +4,14 @@
 
 class Address {
 public:
+    Address() {}
     Address(std::string city, std::string street, int building, int flat);
-
     std::string print_to_file() {
         std::string out = city + ", " + street + ", " + std::to_string(building) +
             ", " + std::to_string(flat);
         return out;
     }
+    std::string compare_city() { return city; }
 
 private:
     std::string city;
@@ -28,7 +29,7 @@ Address::Address(std::string city, std::string street, int building, int flat) {
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    int num = 0, building = 0, flat = 0;
+    unsigned num = 0, building = 0, flat = 0;
     std::string city, street;
     std::ifstream fin("in.txt");
     std::ofstream fout("out.txt");
@@ -39,37 +40,30 @@ int main() {
     fin >> num;
     fout << num << std::endl;
 
-    std::string* arr_address = new std::string[num];
-    std::string string;
+    Address *arr = new Address[num];
     for (int i = 0; i < num; ++i) {
         fin >> city >> street >> building >> flat;
         Address address = Address(city, street, building, flat);
-        std::string full_address = address.print_to_file();
-        arr_address[i] = full_address;
-        //std::cout << address.print_to_file() << std::endl;
+        arr[i] = address;
     }
-    std::cout << std::endl;
 
     bool swapped = false;
     do {
         swapped = false;
         for (int i = num - 1; i > 0; i--) {
-            if (arr_address[i] < arr_address[i - 1]) {
-                std::string temp_address = arr_address[i - 1];
-                arr_address[i - 1] = arr_address[i];
-                arr_address[i] = temp_address;
+            if (arr[i].compare_city() < arr[i - 1].compare_city()) {
+                Address temp_address = arr[i - 1];
+                arr[i - 1] = arr[i];
+                arr[i] = temp_address;
                 swapped = true;
             }
         }
     } while (swapped);
 
     for (int i = 0; i < num; ++i) {
-        std::string string = arr_address[i];
-        fout << string << std::endl;
-        //std::cout << string << std::endl;
+        fout << arr[i].print_to_file() << std::endl;
     }
 
-    delete[] arr_address;
     fin.close();
     fout.close();
     return 0;
